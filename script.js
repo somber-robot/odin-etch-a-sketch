4f1b0970f8706a031d789c150d6ac2190485f211
somber-robot <somberrobot@gmail.com>
2025-06-20 10:39:25 +0100

    Create starting grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const GRID_SIZE = 480;
+const container = document.querySelector("#container");
+
+let grid_count = 16;
+
+for (i = 0; i < (grid_count*grid_count); i++){
+    let square = document.createElement("div");
+    square.style = `border: 1px solid red; 
+                    box-sizing: border-box;
+                    flex: auto;
+                    width: ${GRID_SIZE/grid_count}px;`;
+    container.appendChild(square);
+}
